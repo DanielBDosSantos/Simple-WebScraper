063fd296d882e03c540a1aaa0e33cda681459f74
Daniel dos Santos <44093089+DanielBDosSantos@users.noreply.github.com>
2019-08-17 07:03:19 -0400

    Add files via upload

---
+++ webScraper.py
@@ -0,0 +1,11 @@
+import bs4 as bs
+import urllib.request
+
+sauce = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+for paragraph in soup.find_all('p'):
+    print(paragraph.string)
+
+for url in soup.find_all('a'):
+    print(url.get('href'))
